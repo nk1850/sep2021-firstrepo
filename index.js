a3b4db8869cca1354497362386ec9a4870c5c313
Niraj Kumar <nirajkr5068@gmail.com>
2022-10-07 11:42:08 +0530

    Update the index.js file for hra calculation

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ let person ={
     fname: 'john',
     lname: 'Smith',
     age : 22,
+    basicSal:2345,
     fullName:()=>{
         console.log(lname + ' '+ fname)
+    },
+    hra:()=>{
+        basicSal * 0.2
     }
 }
 person.fullName()
+person.basicSal()
